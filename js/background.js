d6b24357ebed8d958ba10df1d099973f11f93ab2
livelace <o.popov@livelace.ru>
2017-10-21 20:08:15 +0300

    0.0.3

--- js/background.js
+++ js/background.js
@@ -1,31 +1,31 @@
-function send_data (message, page_url, sender, sendResponse) {
+function send_data (message, sender, sendResponse) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        var page_url = tab.url;
 
-    chrome.storage.sync.get(['nerURL', 'nerTimeout'], function (result) {
+        console.log(page_url);
+
+        chrome.storage.sync.get(['nerURL', 'nerTimeout'], function (result) {
         var content = {content: message, url: encodeURIComponent(page_url)}
 
-        $.ajax({
-            url: result.nerURL,
-            type: 'post',
-            dataType: 'json',
-            data: JSON.stringify(content),
-            error: function (jqXHR, textStatus, errorThrown) {
-                sendResponse({status: 'error', code: jqXHR.status, text: errorThrown});
-            },
-            success: function (jqXHR, textStatus, errorThrown) {
-                sendResponse({status: 'success', code: jqXHR.status, text: errorThrown});
-            },
-            timeout: result.nerTimeout
+            $.ajax({
+                url: result.nerURL,
+                type: 'post',
+                dataType: 'json',
+                data: JSON.stringify(content),
+                error: function (jqXHR, textStatus, errorThrown) {
+                    sendResponse({status: 'error', code: jqXHR.status, text: errorThrown});
+                },
+                success: function (jqXHR, textStatus, errorThrown) {
+                    sendResponse({status: 'success', code: jqXHR.status, text: errorThrown});
+                },
+                timeout: result.nerTimeout
+            });
         });
     });
 }
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var tab = tabs[0];
-        var page_url = tab.url;
-        //console.log(page_url);
-
-        send_data(message, page_url, sender, sendResponse);
-        return true;
-    });
+    send_data(message, sender, sendResponse);
+    return true;
 });
